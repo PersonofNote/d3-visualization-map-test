1fe7e36e172111200d7f0e69b96b688044164941
PersonofNote <jtm89@nau.edu>
2019-09-05 15:19:21 -0700

    Refactor to use d3 version 4, and remove dataMaps library

--- index.js
+++ index.js
@@ -1,37 +1,50 @@
+var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
 
-var loaded = false;
 
-//Use library to create map
-var worldMap = new Datamap({
-  element: document.getElementById('map-container'),
-  scope: 'world',
-  geographyConfig: {
-      borderWidth: 0,
-      popupOnHover: false,
-      highlightOnHover: false
-  },
+var projection = d3.geoMercator()
+    .center([2, 47])                
+    .scale(100)                       
+    .translate([ width/2, height/2 ]) //Center
 
-});
-
+// 
+d3.json("http://enjalot.github.io/wwsd/data/world/world-110m.geojson", function(data){  
+    // Draw the map
+    svg.append("g")
+        .selectAll("path")
+        .data(data.features)
+        .enter()
+        .append("path")
+          .attr("fill", "grey")
+          .attr("d", d3.geoPath()
+              .projection(projection)
+          )
+        .style("stroke", "grey")
+})
 
 var rows = [];
-//Dataset obtained from data.gov
 var dataSet = "landslide-test-data-truncated.csv";
 var landslides = d3.csv(dataSet, function(d) {
-  console.log(d.date_);
-  rows.push(d)
+  //console.log(d.date_);
+  //console.log(d.latitude)
   addTomap(d);
+  rows.push(d)
     }, function(error, d) {
-        //Error
+        console.log(rows[2].latitude);
+        //addTomap(rows[2]);
     });
 
 
 function addTomap(d) {
-  d3.select("body").append("p").text(d.date_);
+  svg.selectAll("circle")
+		.data(rows).enter()
+        .append("circle")
+        .attr("class", "dot")
+        .attr("cx",function(d) { return projection([d.longitude,d.latitude])[0]})
+		.attr("cy", function (d) { return projection([d.longitude,d.latitude])[1]})
+		.attr("r", "8px")
 }
-
-
-
 
 
 
