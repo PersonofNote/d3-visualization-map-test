4da6f39b2ab9ed884122535bacc01a4d0c8f65b0
PersonofNote <jtm89@nau.edu>
2019-09-06 08:12:22 -0700

    Set up animation

--- index.js
+++ index.js
@@ -2,37 +2,38 @@ var svg = d3.select("svg"),
     width = +svg.attr("width"),
     height = +svg.attr("height");
 
+var gMap = svg.append("g"); // appended first
+var gDots = svg.append("g");
 
 var projection = d3.geoMercator()
     .center([2, 47])                
     .scale(100)                       
-    .translate([ width/2, height/2 ]) //Center
 
-// 
+const playButton = document.getElementById("playbutton");
+playButton.addEventListener("click", togglePlay);
+
+function drawMap() { 
 d3.json("http://enjalot.github.io/wwsd/data/world/world-110m.geojson", function(data){  
     // Draw the map
-    svg.append("g")
-        .selectAll("path")
+    gMap.selectAll("path")
         .data(data.features)
         .enter()
-        .append("path")
-          .attr("fill", "grey")
+        .append("path")       
           .attr("d", d3.geoPath()
               .projection(projection)
           )
-        .style("stroke", "grey")
 })
+initializeDots();
+}
 
+function initializeDots(){
 var rows = [];
 var dataSet = "landslide-test-data-truncated.csv";
-var landslides = d3.csv(dataSet, function(d) {
-  //console.log(d.date_);
-  //console.log(d.latitude)
+d3.csv(dataSet, function(d) {
   addTomap(d);
   rows.push(d)
-    }, function(error, d) {
-        console.log(rows[2].latitude);
-        //addTomap(rows[2]);
+  //console.log(d.date_)
+    }, function(error, d) {        
     });
 
 
@@ -43,8 +44,64 @@ function addTomap(d) {
         .attr("class", "dot")
         .attr("cx",function(d) { return projection([d.longitude,d.latitude])[0]})
 		.attr("cy", function (d) { return projection([d.longitude,d.latitude])[1]})
-		.attr("r", "8px")
+    .attr("r", "8px")
+    .attr("opacity", 0.3)
 }
+}
+
+drawMap();
+
+
+
+/**
+* Initialize animation variables
+**/
+var frame = 0;
+var startYear = 2011;
+var displayYear = startYear;
+var interval;
+var speed = 300; // ms
+var playing=false;
+var thisYear = 2019;
+
+/*
+* Reimplementing features, not ready yet
+*/ 
+//const timeline = document.getElementById('timeline');
+
+//timeline.addEventListener('mousedown', pause)
+//timeline.addEventListener('mouseup', setYear)
+
+/**
+* Start/stop playback
+**/
+
+function togglePlay() {
+  if ( playing==true ) {
+    playing=false;
+    clearInterval( interval );
+    //addVideo();
+  }
+  else if (playing == false) {
+    interval = setInterval(function () {
+      frame++;
+      displayYear = frame+startYear;
+      //timeline.value = currentYear;
+      console.log(displayYear);
+      if(displayYear >= thisYear){
+        frame = 0;
+      }
+
+     //Change bubble opacity to one if year matches
+      
+    }, speed);
+    //removeVideo();
+    playing=true;
+  }
+}
+
+//makeTickmarks();
+
 
 
 
