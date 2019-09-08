fee8d61ed58dba3ba1e090ea0c320394d8ae4f34
PersonofNote <jtm89@nau.edu>
2019-09-07 22:42:37 -0700

    Animate events according to year

--- index.js
+++ index.js
@@ -26,32 +26,43 @@ d3.json("http://enjalot.github.io/wwsd/data/world/world-110m.geojson", function(
 initializeDots();
 }
 
-function initializeDots(){
 var rows = [];
+
+function initializeDots(){
+
 var dataSet = "landslide-test-data-truncated.csv";
 d3.csv(dataSet, function(d) {
-  addTomap(d);
   rows.push(d)
-  //console.log(d.date_)
-    }, function(error, d) {        
+  //Slightly convoluted way of getting just the year from the dataset
+  var eventDate = d.date_.split(' ');
+  var eventYearArr = eventDate[0].split('/');
+  var eventYear = eventYearArr[2];
+  addTomap(d, eventYear);
+    }, function(error, d) { 
+      //Make timeline       
     });
 
 
-function addTomap(d) {
+function addTomap(d, eventYear) {
   svg.selectAll("circle")
 		.data(rows).enter()
         .append("circle")
-        .attr("class", "dot")
+        .attr("class", `dot dot-${eventYear}`)
         .attr("cx",function(d) { return projection([d.longitude,d.latitude])[0]})
 		.attr("cy", function (d) { return projection([d.longitude,d.latitude])[1]})
-    .attr("r", "8px")
-    .attr("opacity", 0.3)
+    .attr("r", "1px")
+    .attr("opacity", 0)
 }
 }
 
-drawMap();
 
+function addVideo() {
+  //Optionally append video to timeline
+}
 
+function makeTimeline() {
+  //Create draggable timeline
+}
 
 /**
 * Initialize animation variables
@@ -81,6 +92,8 @@ function togglePlay() {
     playing=false;
     clearInterval( interval );
     //addVideo();
+    d3.selectAll('play-button')
+    .text("Pause");
   }
   else if (playing == false) {
     interval = setInterval(function () {
@@ -90,9 +103,19 @@ function togglePlay() {
       console.log(displayYear);
       if(displayYear >= thisYear){
         frame = 0;
+        svg.selectAll("circle")
+          .transition()
+          .attr("opacity", 0);
       }
-
+      //Set play button
+      d3.selectAll('play-button')
+        .text("Pause");
      //Change bubble opacity to one if year matches
+     d3.selectAll(`.dot-${displayYear}`)
+      .transition()
+      .attr("r", "8px")
+      .attr('opacity', 1)
+      .duration(500)
       
     }, speed);
     //removeVideo();
@@ -103,6 +126,13 @@ function togglePlay() {
 //makeTickmarks();
 
 
+/*
+function updateBubble() {
+  if rows.map(x => x * 2) {
+    
+  }
+}
+*/
 
 
-  
+drawMap();
