15e5d51a0e0ae7e869f9be8bc90a7ff498a92f68
jessica-oro <jessica@oro.co>
2019-12-04 09:03:29 -0800

    Add temporary tooltip

--- index.js
+++ index.js
@@ -1,211 +1,201 @@
-const worldMapSvg = d3.select("#worldMap");
+const worldMapSvg = d3.select('#worldMap');
 
-// console.log(svg);
+const gMap = worldMapSvg.append('g'); // appended first so dots are drawn on top rather than behind
+const gDots = worldMapSvg.append('g');
+const tooltip = d3.select('body').append('div')
+  .attr('class', 'tooltip')
+  .style('opacity', 0);
 
-const gMap = worldMapSvg.append("g"); // appended first so dots are drawn on top rather than behind
-const gDots = worldMapSvg.append("g");
+const projection = d3.geoMercator()
+  .center([2, 47])
+  .scale(100);
 
-var projection = d3.geoMercator()
-    .center([2, 47])
-    .scale(100)
-
-const playButton = document.getElementById("playbutton");
-playButton.addEventListener("click", togglePlay);
-const playIcons = document.getElementsByClassName("icon-controls");
+const playButton = document.getElementById('playbutton');
+playButton.addEventListener('click', togglePlay);
+const playIcons = document.getElementsByClassName('icon-controls');
 const timeline = document.getElementById('timeline');
-const yearDiv = document.getElementById('frame');
+const yearDiv = document.getElementById('frame'); // Haven't added the div to display the year yet
+
+/**
+ * Initialize animation variables
+ * */
+let frame = 0;
+const startYear = 1776;
+let displayYear = startYear;
+let interval;
+const speed = 50; // ms TODO: Add variable speed to playback
+let playing = false;
+const thisYear = new Date().getFullYear();
 
 timeline.oninput = function () {
-    // console.log(this.value);
-    pause();
-    clearAll();
-    displayYear = this.value;
-    frame = this.value - startYear;
-    for (i = 0; i < (this.value - startYear); i++)
-        updateDots(i + startYear);
-}
+  pause();
+  clearAll();
+  displayYear = this.value;
+  frame = this.value - startYear;
+  for (let i = 0; i < (this.value - startYear); i++) { updateDots(i + startYear); }
+};
 
 
 function drawMap() {
-    d3.json("https://PersonofNote.github.io/d3-visualization-map-test/world-110m.geojson", function (data) {
-        gMap.selectAll("path")
-            .data(data.features)
-            .enter()
-            .append("path")
-            .attr("class", `landpath`)
-            .attr("d", d3.geoPath()
-                .projection(projection)
-            )
-    })
-    initializeDots();
+  d3.json('https://PersonofNote.github.io/d3-visualization-map-test/world-110m.geojson', (data) => {
+    gMap.selectAll('path')
+      .data(data.features)
+      .enter()
+      .append('path')
+      .attr('class', 'landpath')
+      .attr('d', d3.geoPath()
+        .projection(projection))
+      .on('mouseover', (d) => {
+        tooltip.transition()
+          .duration(200)
+          .style('opacity', 0.9);
+        tooltip.html(d.properties.name)
+          .style('left', `${d3.event.pageX }px`)
+          .style('top', `${d3.event.pageY - 28 }px`);
+      })
+      .on('mouseout', (d) => {
+        tooltip.transition()
+          .duration(500)
+          .style('opacity', 0);
+      });
+  });
+  initializeDots();
 }
 
 
-var rows = [];
-var events = [];
+const rows = [];
+const events = []; // Placeholder for adding videos
 
 function addEvents() {
-    const eventData = "world-events.csv";
-    d3.csv(eventData, function (d) {}, function (error, d) {});
+  const eventData = 'world-events.csv';
+  d3.csv(eventData, (d) => {}, (error, d) => {});
 }
 
 
 function initializeDots() {
-
-    var dataSet = "embassy-data.csv";
-    d3.csv(dataSet, function (d) {
-        rows.push(d);
-        addTomap(d);
-    }, function (error, d) {
-        fillTimeline();
-    });
+  const dataSet = 'embassy-data.csv';
+  d3.csv(dataSet, (d) => {
+    rows.push(d);
+    addTomap(d);
+  }, (error, d) => {
+    fillTimeline();
+  });
 }
 
 function fillTimeline() {
-    const years = document.getElementById("yearsList")
-    for (let i = 0; i < ((thisYear - startYear) + 1); i++) {
-        var value = i + startYear;
-        var year = document.createElement('option');
-        year.value = value;
-        if (value == startYear || value == thisYear || value == (Math.floor((thisYear - startYear) / 2) + startYear)) {
-            year.label = year.value;
-            year.class = "visible";
-        }
-        years.appendChild(year);
+  const years = document.getElementById('yearsList');
+  for (let i = 0; i < ((thisYear - startYear) + 1); i++) {
+    const value = i + startYear;
+    const year = document.createElement('option');
+    year.value = value;
+    if (value == startYear || value == thisYear || value == (Math.floor((thisYear - startYear) / 2) + startYear)) {
+      year.label = year.value;
+      year.class = 'visible';
     }
+    years.appendChild(year);
+  }
 }
 
 function addTomap(d) {
-    worldMapSvg.selectAll('circle')
-        .data(rows).enter()
-        .append("circle")
-        .attr("class", `dot dot-${d.year} ${d.country}`)
-        .attr("data-event", d.event)
-        .attr("data-country", d.country)
-        .attr("cx", function (d) {
-            return projection([d.lon, d.lat])[0];
-        })
-        .attr("cy", function (d) {
-            return projection([d.lon, d.lat])[1]
-        })
-        .attr("r", "6px")
-        .attr("fill", "blue")
-        .attr("opacity", 0)
+  worldMapSvg.selectAll('circle')
+    .data(rows).enter()
+    .append('circle')
+    .attr('class', `dot dot-${d.year} ${d.country}`)
+    .attr('data-event', d.event)
+    .attr('data-country', d.country)
+    .attr('cx', (d) => projection([d.lon, d.lat])[0])
+    .attr('cy', (d) => projection([d.lon, d.lat])[1])
+    .attr('r', '2px')
+    .attr('fill', 'blue')
+    .attr('opacity', 0);
 }
 
 
 function updateDots(year) {
-    var selectDots = worldMapSvg.selectAll(`.dot-${year}`);
-    //Set all dots for the year to 0 opacity
-    selectDots.each(function(){
-        var targetDot = worldMapSvg.selectAll(`.${this.dataset.country}`)
-        targetDot
-        .transition()
-        .attr("opacity", 0)
-        .duration(500)
+  const selectDots = worldMapSvg.selectAll(`.dot-${year}`);
+  // Set all dots for the year to 0 opacity
+  selectDots.each(function () {
+    const targetDot = worldMapSvg.selectAll(`.${this.dataset.country}`);
+    targetDot
+      .transition()
+      .attr('opacity', 0)
+      .duration(500);
+  });
+  // Change bubble opacity to one if year matches
+  selectDots
+    .transition()
+    .attr('r', (d) => {
+      if (d.event.includes('legation')) {
+        return '6px';
+      } if (d.event.includes('embassy')) {
+        return '8px';
+      } if (d.event.includes('closure')) {
+        return '1px';
+      }
     })
-    //Change bubble opacity to one if year matches
-    selectDots
-        .transition()
-        /*
-        .attr("r", function (d) {
-            console.log(d);
-            if (d.event.includes("legation")) {
-                // console.log("legation");
-                return "6px";
-            } else if (d.event.includes("embassy")) {
-                // console.log("embassy");
-                return "8px";
-            } else if (d.event.includes("closure")) {
-                // console.log("closure");
-                return "1px";
-            } else {
-                return;
-            }
-        })
-        */
-        .attr("opacity", function (d) {
-            if (d.event.includes("legation")) {
-                return 0.5;
-            } else if (d.event.includes("embassy")) {
-                return 1;
-            } else if (d.event.includes("closure")) {
-                return 0;
-            } else {
-                return;
-            }
-        })
-
+    .attr('opacity', (d) => {
+      if (d.event.includes('legation')) {
+        return 0.5;
+      } if (d.event.includes('embassy')) {
+        return 1;
+      } if (d.event.includes('closure')) {
+        return 0;
+      }
+    });
 }
 
 function clearAll() {
-    frame = 0;
-    worldMapSvg.selectAll("circle")
-        .transition()
-        .attr("opacity", 0);
+  frame = 0;
+  worldMapSvg.selectAll('circle')
+    .transition()
+    .attr('opacity', 0);
 }
 
 function swapIcons() {
-    for (let icon of playIcons) {
-        icon.classList.toggle("visible");
-    }
-
+  for (const icon of playIcons) {
+    icon.classList.toggle('visible');
+  }
 }
 
 function addVideo() {
-    //Optionally append video to timeline
+  // Optionally append video to timeline
 }
-
-
-/**
- * Initialize animation variables
- **/
-var frame = 0;
-var startYear = 1776;
-var displayYear = startYear;
-var interval;
-var speed = 50; // ms TODO: Add variable speed to playback
-var playing = false;
-var thisYear = new Date().getFullYear();
 
 
 /**
  * Start/stop playback
- **/
+ * */
 
 function togglePlay() {
-    if (playing == true) {
-        //playButton.innerHTML = "Play";
-        playing = false;
-        clearInterval(interval);
-        //addVideo();
-
-    } else if (playing == false) {
-        interval = setInterval(function () {
-            frame++;
-            updateDots(displayYear);
-            displayYear = frame + startYear;
-            timeline.value = displayYear;
-            if (displayYear >= thisYear) {
-                clearAll();
-            }
-
-        }, speed);
-        //removeVideo();
-        playing = true;
-    }
-    swapIcons();
+  if (playing == true) {
+    // playButton.innerHTML = "Play";
+    playing = false;
+    clearInterval(interval);
+    // addVideo();
+  } else if (playing == false) {
+    interval = setInterval(() => {
+      frame++;
+      updateDots(displayYear);
+      displayYear = frame + startYear;
+      timeline.value = displayYear;
+      if (displayYear >= thisYear) {
+        clearAll();
+      }
+    }, speed);
+    // removeVideo();
+    playing = true;
+  }
+  swapIcons();
 }
 
 function pause() {
-    if (playing == true) {
-        playing = false;
-        clearInterval(interval);
-        swapIcons();
-        //TODO: add popup saying what year it is
-    }
+  if (playing == true) {
+    playing = false;
+    clearInterval(interval);
+    swapIcons();
+    // TODO: add popup saying what year it is
+  }
 }
-
 
 
 drawMap();
